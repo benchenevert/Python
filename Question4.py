461c7c8aeb1cfe0f9603fd393865b99d5c17d659
benchenevert <ben.chenevert@wsu.edu>
2022-07-21 00:32:55 -0700

    Yikes

---
+++ Question4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 00:18:13 2022
+
+@author: benchenevert
+"""
+
+def function():
+    def midpoint(f, a, b, n):
+    
+        callable(midpoint(f, a, b, n))
+    
+        total = 0
+        h = (b-a)/n         
+      
+           
+        for i in range(0,n):
+            
+            total  += function(((a + (h * i) + (a + (h * (i + 1))))))
+                   
+ 
+    
+  
+        return (total * h)
+    
+ # I have been screaming at my laptop for far too long
+        
+        
+    
